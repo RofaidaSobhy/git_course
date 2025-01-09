4f215dc849dffd1198283d8924f92363d907a706
Rofaida <rofaidasobhy123@gmail.com>
2025-01-09 10:38:36 +0200

    printHelloGit() is added to HelloITI.java

--- HelloITI.java
+++ HelloITI.java
@@ -2,7 +2,12 @@ public class HelloITI{
 	public void printHelloITI(){
 		System.out.print("Hello ITI");
 	}
+	public void printHelloGit(){
+		System.out.print("Hello Git");
+	}
+
 	public void main (String[] args){
-		printHelloITI();	
+		printHelloITI();
+		printHelloGit();	
 	}
 }
